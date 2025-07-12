04351b627d9cb774d9ae1d5e7f31f08c0ddc6e77
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-12 17:40:41 +0530

    top button added

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import 'aos/dist/aos.css';
 import { useEffect } from "react";
 import DoctorProfile from "./components/DoctorProfile";
 import Blog from "./pages/Blog";
+import ScrollToTopButton from "./components/ScrollTop";
 
 function App() {
   useEffect(() => {
@@ -36,6 +37,7 @@ function App() {
             <Route path="*" element={<Home />} />
           </Routes>
           <WhatsAppButton/>
+          <ScrollToTopButton/>
         </main>
         <Footer />
       </div>
