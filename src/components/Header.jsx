4843b764defa29f70fa27e5aa4b0cc6744c09956
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-14 12:05:43 +0530

    Header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
       <div className="topbar d-flex justify-content-between align-items-center px-3 py-2 text-white flex-wrap">
         {/* Hidden on small screens */}
         <div className="d-none d-md-flex align-items-center gap-3">
-          <span><FaPhoneAlt className="me-2" />04924 246 150</span>
-          <span><FaMapMarkerAlt className="me-2" /> Kalladikode, Karimba South, Kerala 678596</span>
+          <a href='tel:04924246150' style={{textDecoration:'none'}}><span><FaPhoneAlt className="me-2" />04924 246 150</span></a>
+          <a href='https://maps.app.goo.gl/ody6gV1iyVLr1LQo6' style={{textDecoration:'none'}} target='_blank'><span><FaMapMarkerAlt className="me-2" /> Kaipamangalam, Aravushala Rd, Moonnupeedika, Kerala 680681</span></a>
         </div>
 
         {/* Always Visible: Social */}
