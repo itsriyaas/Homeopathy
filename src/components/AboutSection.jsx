c00de027842c9a5d71ba6145a1fe0c91d8282a69
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-30 11:38:02 +0530

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -50,7 +50,7 @@ const AboutUsSection = () => {
                             className="img-fluid rounded-4 shadow"
                             style={{ maxHeight: '400px', objectFit: 'cover' }}
                         />
-                        <p className='mt-5 fst-italic'>"Being born into a family of healers, I've seen firsthand the power of homeopathy. I carry forward this legacy with dedication, empathy, and modern clinicalinsight." <br />- Dr. Aiswarya M.S</p>
+                        <p className='mt-5 fst-italic'>"Being born into a family of healers, I've seen firsthand the power of homeopathy. I carry forward this legacy with dedication, empathy, and modern clinical insight." <br />- Dr. Aiswarya M.S</p>
                     </div>
                     {/* Text Column */}
                     <div className="col-md-6" data-aos="fade-right">
@@ -61,10 +61,7 @@ const AboutUsSection = () => {
                         <p className="text-muted">
                             At our clinic, we are committed to offering compassionate, holistic, and evidence-based homeopathic care to children. Led by <strong>Dr. Aiswarya</strong>, a dedicated pediatric homeopathic physician, we specialize in treating a wide range of childhood conditions with a personalized and gentle approach.
                         </p>
-                        <p className="text-muted">
-                           <strong>Dr. Aiswarya M.S</strong> is a skilled and compassionate Homoeopathic Paediatrician with over 6 years of clinical experience, known for her dedicated work in treating pediatric neurological, behavioral, and respiratory disorders. She holds a B.H.M.S and M.D in Paediatrics from Sarada Krishna Homoeopathic Medical College, Tamil Nadu, and also holds a Diploma in Yoga, reflecting her holistic approach to wellness. Dr. Aiswarya was honored with the Best Researcher Award by the Central Council for Research in Homoeopathy (CCRH), New Delhi, for her groundbreaking thesis on the Effectiveness of Homoeopathy in the Treatment of Epilepsy Among Children, successfully treating over 30 pediatric cases of epileptiform fits with homoeopathic remedies. She has extensive clinical experience in treating childhood allergies and asthma, focusing on Permanent cure through individualized homeopathic treatment.
-
-                        </p>
+                        
                         <p className='text-muted fw-bold'>Specialisations :</p>
                         <ul>
                             <li>Allergy</li>
